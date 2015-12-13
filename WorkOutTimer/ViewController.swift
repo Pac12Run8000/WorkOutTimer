473a26138dc6425fdc4c285d1e48d2b486cd6c79
Pac12Run8000 <norbert298@gmail.com>
2015-12-13 08:28:19 -0800

    version 0.02

--- WorkOutTimer/ViewController.swift
+++ WorkOutTimer/ViewController.swift
@@ -11,9 +11,11 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     var timer = NSTimer()
-    let minutes:Int = 0
-    let seconds:Int = 0
-    let fraction:Int = 0
+    var minutes:Int = 0
+    var seconds:Int = 0
+    var fraction:Int = 0
+    var stopWatchString:String = ""
+    var laps:[String] = []
     
     var startStopWatch:Bool = true
     var addLap:Bool = false
@@ -22,6 +24,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBOutlet weak var lblStartStop: UIButton!
     @IBOutlet weak var lblLapReset: UIButton!
     @IBOutlet weak var lblStopWatchDisplay: UILabel!
+    @IBOutlet weak var tblviewLaps: UITableView!
     
     @IBAction func startStopAction(sender: AnyObject) {
         if (startStopWatch == true) {
@@ -39,6 +42,39 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }
     }
     @IBAction func lapResetAction(sender: AnyObject) {
+        if (addLap == true) {
+            laps.insert(stopWatchString, atIndex: 0)
+            tblviewLaps.reloadData()
+        } else {
+            addLap = false
+            laps.removeAll(keepCapacity: false)
+            tblviewLaps.reloadData()
+            self.lblLapReset.setTitle("Reset", forState: UIControlState.Normal)
+            fraction = 0
+            minutes = 0
+            seconds = 0
+            stopWatchString = "00:00:00"
+            self.lblStopWatchDisplay.text = stopWatchString
+        }
+    }
+    
+    func updateStopWatch() {
+        fraction += 1
+        
+        if (fraction == 100) {
+            seconds += 1
+            fraction = 0
+        }
+        if (seconds == 60) {
+            minutes += 1
+            seconds = 0
+        }
+        
+        let fractionString = fraction > 9 ? "\(fraction)" : "0\(fraction)"
+        let secondString = seconds > 9 ? "\(seconds)" : "0\(seconds)"
+        let minutesString = minutes > 9 ? "\(minutes)" : "0\(minutes)"
+        stopWatchString = "\(minutesString):\(secondString):\(fractionString)"
+        self.lblStopWatchDisplay.text = stopWatchString
     }
     
     override func viewDidLoad() {
@@ -51,6 +87,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         self.lblLapReset.layer.borderColor = UIColor.blackColor().CGColor
         self.lblLapReset.layer.cornerRadius = self.lblLapReset.frame.size.width / 2
         
+        self.lblStopWatchDisplay.text = "00:00:00"
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -61,13 +99,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: UITableViewCellStyle.Value1, reuseIdentifier: "Cell")
         cell.backgroundColor = self.view.backgroundColor
-        cell.textLabel?.text = "Lap"
-        cell.detailTextLabel!.text = "00:00:00"
+        cell.textLabel?.text = "Lap \(laps.count - indexPath.row)"
+        cell.detailTextLabel!.text = laps[indexPath.row]
         return cell
         
     }
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return laps.count
     }
 
 }
